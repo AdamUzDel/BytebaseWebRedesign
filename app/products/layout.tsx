e6ac8de8f28007f946b3774ce907f606ce448aa4
AdamUzDel <adamssemakula@gmail.com>
2025-01-28 23:29:37 +0300

    Optimizing UI for mobile

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -3,9 +3,7 @@ import { ReactNode } from 'react'
 export default function ProductsLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <main className="container mx-auto">
         {children}
-      </main>
     </div>
   )
 }
